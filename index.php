65434a9cc0b7aaa46695e61162f8dc29c24ece9d
Leochan <lleonardanton@gmail.com>
2024-04-03 21:11:45 +0200

    Parce que c'est notre projet

--- index.php
+++ index.php
@@ -1,19 +1,28 @@
 <!doctype html>
 <html lang="fr">
 <head>
-  <meta charset="utf-8">
-  <title>AutoIUT</title>
-  <link rel="stylesheet" href="style_homepage.css">
-  <script src="script.js"></script>
+    <meta charset="utf-8">
+    <title>AutoIUT</title>
+    <link rel="stylesheet" href="style_homepage.css">
+    <script src="script.js"></script>
+    <link rel="icon" href="/assets/imgs/favicon.png" type="image/png">
 </head>
 <body>
-  <nav>
-    <a href="">Accueil</a>
-    <a href="">Nos véhicules</a>
-    <a href="">Mon Compte</a>
-    <a href="">Créer une annonce</a>
-    <a href="">Panier</a>
-    
-  </nav>
+    <header>  
+        <nav class="menu">
+            <ul class="link-container">
+                <a href="https://projetr209.nekocorp.fr/index.html"><img src="assets/imgs/logo.png" class="home-picture" alt="Logo"></a>
+                <li><a href="">Accueil</a></li>
+                <li><a href="">Nos véhicules</a></li>
+                <li><a href="">Panier</a></li>
+                <li><a href="">A propos</a></li>
+            </ul>
+            <div class="account">
+                <a href="">Mon compte</a>
+            </div>
+        </nav>
+    </header>
+    <p class="welcome">Bienvenue sur Auto Télécom, le site de revente de voiture de l'IUT de Mont de Marsan.<br>
+    Sur ce site, vous retrouverez toutes nos meilleure occasions disponibles à la vente, obtenues de façon bien evidamment légale 🙂</p>  
 </body>
 </html>
