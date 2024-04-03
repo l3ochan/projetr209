8c42678a81d4a6f37cbd8865c0779bbde6199413
Leochan <lleonardanton@gmail.com>
2024-04-03 18:57:10 +0200

    Enfin on commence mdr

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Test PHP</title>
-    </head>
-    <body>
-        <?php echo '<p>Bonjour le monde</p>'; ?>
-    </body>
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>AutoIUT</title>
+  <link rel="stylesheet" href="style_homepage.css">
+  <script src="script.js"></script>
+</head>
+<body>
+  <nav>
+    <a href="">Accueil</a>
+    <a href="">Nos véhicules</a>
+    <a href="">Mon Compte</a>
+    <a href="">Créer une annonce</a>
+    <a href="">Panier</a>
+    
+  </nav>
+</body>
 </html>
