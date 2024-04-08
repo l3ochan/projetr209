6cf93a86c422503bf828e51ffbf417ed48fcb918
Leochan <lleonardanton@gmail.com>
2024-04-08 17:35:50 +0200

    Ca alarcge

--- index.php
+++ index.php
@@ -17,6 +17,17 @@
     ?>
 
     <!-- Contenu spécifique à chaque page -->
+    <?php
+    // Récupérer l'URL demandée
+    $req_url = $_SERVER['REQUEST_URI'];
+
+    // Vérifier si l'URL demandée est "/"
+    if ($req_url === "/") {
+        // Rediriger vers la page d'accueil
+        header("Location: https://projetr209.nekocorp.fr/index.php?page=home");
+        exit(); // Arrêter l'exécution du script après la redirection
+    }
+    ?>
     <?php if ($page === 'home') : ?>
         <p class="welcome">Bienvenue sur Auto Télécom, le site de revente de voiture de l'IUT de Mont de Marsan.<br>
         Sur ce site, vous retrouverez toutes nos meilleure occasions disponibles à la vente, obtenues de façon bien evidamment légale 🙂</p>  
@@ -27,12 +38,13 @@
         <p class="basket">Corps de texte du panier</p>
     <?php elseif ($page === 'about-us') : ?>
         <p class="about-us">Corps de texte du a propos</p>
+        <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/3vUxlLXE7zyNS4SpRK2PjT?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
     <?php elseif ($page === 'login') : ?>
         <p class="login">Corps de texte de la page de login</p>    
     <?php else : ?>
         <div class="error404">
-            <h1>Error 404, Page not found</h1>
             <img src="/assets/imgs/404.png" class="404 error picture" alt="404 error picture">
+            <h1>404 - Page Not Found</h1><br>
         </div>
         <?php endif; ?>
 
