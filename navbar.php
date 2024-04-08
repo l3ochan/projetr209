9c5006b7b794efd00875cf8b707ca29c3b71865a
Leochan <lleonardanton@gmail.com>
2024-04-08 16:52:53 +0200

    aa

--- navbar.php
+++ navbar.php
@@ -5,11 +5,11 @@
 </head>
 <nav class="menu">
             <ul class="link-container">
-                <a href="https://projetr209.nekocorp.fr/index.php"><img src="assets/imgs/logo.png" class="home-picture" alt="Logo"></a>
-                <li><a href="index.php?page=accueil">Accueil</a></li>
+                <a href="index.php?page=home"><img src="assets/imgs/logo.png" class="home-picture" alt="Logo"></a>
+                <li><a href="index.php?page=home">Accueil</a></li>
                 <li><a href="index.php?page=itemlist">Nos véhicules</a></li>
-                <li><a href="index.php?page=panier">Panier</a></li>
-                <li><a href="index.php?page=a-propos">A propos</a></li>
+                <li><a href="index.php?page=basket">Panier</a></li>
+                <li><a href="index.php?page=about-us">A propos</a></li>
             </ul>
             <div class="account">
                 <a href="index.php?page=login">Se connecter</a>
