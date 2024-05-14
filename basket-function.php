d9df8d9d7e38f5bb7cd9983a5b8b607abcab985e
Arthamir <pierremicha@orange.fr>
2024-05-14 08:35:20 +0200

    bababa

--- basket-function.php
+++ basket-function.php
@@ -8,7 +8,6 @@ function creationPanier(){
    if (!isset($_SESSION['panier'])){
       $_SESSION['panier']=array();
       $_SESSION['panier']['libelleProduit'] = array();
-      $_SESSION['panier']['qteProduit'] = array();
       $_SESSION['panier']['prixProduit'] = array();
       $_SESSION['panier']['verrou'] = false;
    }
--- item-list.php
+++ item-list.php
@@ -30,6 +30,9 @@
                     echo "</a>";
                     echo "</div>";
                     echo "</div>";
+                    echo "<a href="basket.php?action=ajout&amp;l=LIBELLEPRODUIT&amp;p=PRIXPRODUIT" onclick="window.open(this.href, '', 
+                    'toolbar=no, location=no, directories=no, status=yes, scrollbars=yes, resizable=yes, copyhistory=no, width=600, height=350'); return false;">Ajouter au panier</a>";
+
                 }
             } else {
                 echo "<p>Aucune donnée trouvée dans la table 'items'.</p>";
