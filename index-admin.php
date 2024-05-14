478bed274d58b58f33a7930ec3a62b22e4290f51
Arthamir <pierremicha@orange.fr>
2024-05-14 14:59:44 +0200

    bababa

---
+++ index-admin.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>Page administrateur</title>
+        <link rel="stylesheet" href="style_homepage.css">
+        <script src="script.js"></script>
+        <link rel="icon" href="/assets/imgs/favicon.png" type="image/png">
+    </head>
+
+<body>
+    <header>
+        <?php
+        include 'navbar-admin.php'
+        include 'config/db_connector.php'
